9deb63218111eed1e6642cb5a284311d356a92ad
unline2 <liujunbingdea4@163.com>
2020-01-05 18:40:58 +0800

    init project, finish building the spring framework

---
+++ src/main/java/me/ffis/randomImage/RandomImageApplication.java
@@ -0,0 +1,15 @@
+package me.ffis.randomImage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by fanfan on 2020/01/05.
+ */
+
+@SpringBootApplication
+public class RandomImageApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RandomImageApplication.class, args);
+    }
+}
